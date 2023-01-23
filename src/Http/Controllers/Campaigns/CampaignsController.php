7abed37972900b180d8edd8c1bfe8b89b184abc9
Michael Wamae <mwamae@cytonn.com>
2023-01-23 15:18:32 +0300

    reduce the listed campaigns to 6

--- src/Http/Controllers/Campaigns/CampaignsController.php
+++ src/Http/Controllers/Campaigns/CampaignsController.php
@@ -63,7 +63,7 @@ class CampaignsController extends Controller
     {
         $workspaceId = Sendportal::currentWorkspaceId();
         $params = ['draft' => true];
-        $campaigns = $this->campaigns->paginate($workspaceId, 'created_atDesc', ['status'], 25, $params);
+        $campaigns = $this->campaigns->paginate($workspaceId, 'created_atDesc', ['status'], 6, $params);
 
         return view('sendportal::campaigns.index', [
             'campaigns' => $campaigns,
